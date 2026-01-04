6b20b1093df3a713e82c3fa5f7d580b9bb34acc6
Belgthor <44882812+Belgthor@users.noreply.github.com>
2026-01-04 00:27:18 -0600

    changed jwt

--- backend/middleware/authConfig.js
+++ backend/middleware/authConfig.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 export const authConfig = {
-  secret: process.env.JWT_SECRET || "roogna-secret-key",
-  refresh: process.env.JWT_REFRESH || "roogna-refresh-key"
+  secret: process.env.JWT_SECRET || "my-secret-key",
+  refresh: process.env.JWT_REFRESH || "my-refresh-key"
 };
